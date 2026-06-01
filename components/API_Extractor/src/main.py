796f40ccf1dc32c3e14f75aab91b839dc54af999
TGossmanDev <thomasgossmandev@gmail.com>
2026-05-31 22:53:17 -0400

    started with api extractor

---
+++ components/API_Extractor/src/main.py
@@ -0,0 +1,12 @@
+import requests
+
+
+
+url = "https://jobright.ai/swan/mini-sites/list?position=0&count=50"
+
+payload = {"category":"newgrad:us:swe","excludeTitle":["AI Trainer","AI Tutor","AI Training","AI Coach","AI Reviewer","AI Rater","AI Content Evaluator","Search Quality Rater","Ads Quality Rater","Annotator","Annotation Specialist","Data Annotation","AI Annotation","Data Labeler","Data Labeling","Labeler","AI Data Specialist","Data Collector","Data Collection","Prompt Optimization","Prompt Creator"],"excludedTitle":["AI Trainer","AI Tutor","AI Training","AI Coach","AI Reviewer","AI Rater","AI Content Evaluator","Search Quality Rater","Ads Quality Rater","Annotator","Annotation Specialist","Data Annotation","AI Annotation","Data Labeler","Data Labeling","Labeler","AI Data Specialist","Data Collector","Data Collection","Prompt Optimization","Prompt Creator"],"location":["San Antonio"]}
+
+response = requests.post(url, json=payload)
+
+print(response.status_code)
+print(response.json())
